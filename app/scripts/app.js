e730db6e6aaf026847d6e26151662c6463417012
Thamizhselvan <thamizhtechie@gmail.com>
2018-04-04 08:19:36 +0530

    init commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    '720kb.datepicker'
   ])
   .config(function ($routeProvider) {
     $routeProvider
